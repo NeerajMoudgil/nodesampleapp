c33e4a7495bd58cb8b737d120d24e41385568ad5
neeraj <neerajcse7@gmail.com>
2018-09-19 17:41:59 +0530

    2 commit

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,5 +3,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //mongoose.connect(process.env.MONGODB_URI);
 //mongoose.connect('mongodb://localhost/Green');
-mongoose.connect('mongodb://139.59.89.13:27017/Green');
+mongoose.connect('mongodb://139.59.89.13:27017/Green').then().catch((err)=>{
+  console.log("mongoose err",err);
+});
 module.exports = {mongoose};
